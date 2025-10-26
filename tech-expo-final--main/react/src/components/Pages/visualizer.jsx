071eeb5b0a2534f527fdc41bac663698b8812fb8
simranjitkaur-2007 <simran785917@gmail.com>
2025-10-26 19:58:48 +0530

    Update visualizer.jsx

--- tech-expo-final--main/react/src/components/Pages/visualizer.jsx
+++ tech-expo-final--main/react/src/components/Pages/visualizer.jsx
@@ -1,17 +1,22 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
-import Codeeditor from './Editor/Codeeditor'
-import theme from './Editor/theme'
+import Codeeditor from "./Editor/Codeeditor";
+import theme from "./Editor/theme";
 
 const Visualizer = () => {
   return (
-
     <ChakraProvider theme={theme}>
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={2}>
-      <Codeeditor />
-    </Box>
-
+      <Box
+        minH="100vh"
+        bg="#0f0a19"
+        color="gray.200"
+        px={6}
+        py={4}
+        display="flex"
+        flexDirection="column"
+      >
+        <Codeeditor />
+      </Box>
     </ChakraProvider>
-
   );
 };
 
